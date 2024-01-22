08423b61ffefd16654cf416a876870e9c9fab28f
Samandar-Rajabboyev <89295109+Samandar-Rajabboyev@users.noreply.github.com>
2024-01-22 23:07:10 +0500

    initialized project, configured typeorm,created hashService, written sign-in and sign-up requests.

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeormConfig from './config/typeorm.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { IamModule } from './iam/iam.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeormConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+    CoffeesModule,
+    UsersModule,
+    IamModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/iam/authentication/authentication.service.ts
@@ -0,0 +1,59 @@
+import {
+  ConflictException,
+  Injectable,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from '../hashing/hashing.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = this.usersRepository.create({
+        email: signUpDto.email,
+        password: await this.hashingService.hash(signUpDto.password),
+      });
+
+      await this.usersRepository.save(user);
+    } catch (err) {
+      const pgUniqueViolationErrorCode = '23505';
+      if (err.code === pgUniqueViolationErrorCode) {
+        throw new ConflictException();
+      }
+      throw err;
+    }
+  }
+
+  async signIn(signInDto: SignInDto) {
+    try {
+      const user = await this.usersRepository.findOneBy({
+        email: signInDto.email,
+      });
+      if (!user) {
+        throw new UnauthorizedException('Unable to sign in');
+      }
+      const isEqual = await this.hashingService.compare(
+        signInDto.password,
+        user.password,
+      );
+      if (!isEqual) {
+        throw new UnauthorizedException('Unable to sign in');
+      }
+      return true;
+    } catch (err) {
+      Logger.error(err);
+      throw new UnauthorizedException('Unable to sign in');
+    }
+  }
+}
